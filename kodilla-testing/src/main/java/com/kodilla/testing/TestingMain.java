632fb8da6097ae74f34d3c02e09e438ff37c3416
MaciejBienia <maciej.bienia79@gmail.com>
2018-01-09 18:52:42 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,20 +1,33 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
 import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        //Calculator test
         Calculator calculator = new Calculator();
 
         String positiveResult = "test OK";
         String negativeResult = "Error!";
-        int result = 7;
+        int calcResult = 7;
 
-        if (calculator.add(2, 5) == result){
+        if (calculator.add(2, 5) == calcResult){
             System.out.println(positiveResult);
         } else {
             System.out.println(negativeResult);
         }
-        if (calculator.substract(11, 4) == result){
+        if (calculator.substract(11, 4) == calcResult){
             System.out.println(positiveResult);
         } else {
             System.out.println(negativeResult);
