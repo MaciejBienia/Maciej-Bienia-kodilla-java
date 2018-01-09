d5d3c5c39eca1b9611d973b81a67456c4b7d6924
MaciejBienia <maciej.bienia79@gmail.com>
2018-01-09 18:14:58 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,23 @@
 package com.kodilla.testing;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        Calculator calculator = new Calculator();
+
+        String positiveResult = "test OK";
+        String negativeResult = "Error!";
+        int result = 7;
+
+        if (calculator.add(2, 5) == result){
+            System.out.println(positiveResult);
+        } else {
+            System.out.println(negativeResult);
+        }
+        if (calculator.substract(11, 4) == result){
+            System.out.println(positiveResult);
+        } else {
+            System.out.println(negativeResult);
+        }
     }
 }
