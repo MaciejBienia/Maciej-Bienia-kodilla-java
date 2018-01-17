6b220795bba3f6566c8674892668a542fc8c281a
MaciejBienia <maciej.bienia79@gmail.com>
2018-01-17 17:49:04 +0100

    First Commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.stream;
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -10,5 +11,8 @@ public class StreamMain {
         poemBeautifier.beautify("Paweł ma papugę", text -> ">>>" + text + "<<<");
         poemBeautifier.beautify("Ania ma królika", text -> text.toUpperCase());
         poemBeautifier.beautify("Ania ma królika", text -> text.replace("a", "A"));
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
