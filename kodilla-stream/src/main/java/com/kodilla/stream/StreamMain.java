4f38606361048d8d124ac8091a7fbb524b1e77b2
MaciejBienia <maciej.bienia79@gmail.com>
2018-01-19 17:21:48 +0100

    First Commit

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -8,11 +8,10 @@ import java.util.stream.Collectors;
 public class StreamMain {
     public static void main(String[] args) {
         Forum forum = new Forum();
-        LocalDate seventeenthJan1998 = LocalDate.of(1998, 1, 17);
 
         Map<Integer, ForumUser> theResultMap = forum.getUserList().stream()
                 .filter(forumUser -> forumUser.getSex() == 'M')
-                .filter(forumUser -> forumUser.getDateOfBirth().isBefore(seventeenthJan1998))
+                .filter(forumUser -> forumUser.getDateOfBirth().isBefore(LocalDate.of(1998, 1, 17)))
                 .filter(forumUser -> forumUser.getPostsPublished() > 0)
                 .collect(Collectors.toMap(ForumUser::getUserId, forumUser -> forumUser));
 
